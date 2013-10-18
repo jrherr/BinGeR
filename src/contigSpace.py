f02b1a3670848aed1e05cc736c793466cebb1398
luo-chengwei <ChengweiLuo@ipsec-143-215-17-137.vpn.gatech.edu>
2013-10-18 17:11:02 +0700

    fix

--- src/contigSpace.py
+++ src/contigSpace.py
@@ -1146,7 +1146,7 @@ class ContigSpace(nx.Graph):
 		
 		results = []
 		for inputSet in inputSets:
-			results.append(radiusKNN(inputSet, labels, radiusNeighbor))
+			results.append(radiusKNN([inputSet, labels, radiusNeighbor]))
 		# interpret the results
 		for result in results.get():
 			for x in result:
