bdb3fbb43e92ad431f873587b4ad854e2760d9c8
chengwei luo <luo.chengwei@icloud.com>
2013-10-23 12:13:45 -0400

    debug tracking

--- src/contigSpace.py
+++ src/contigSpace.py
@@ -1150,18 +1150,17 @@ class ContigSpace(nx.Graph):
 			pfile = projInfo.out_dir + '/temp.' + str(i+1)
 			pfiles.append(pfile)
 		
-		"""
+		
 		pool = mp.Pool(options.num_proc)
 		cmds = [[s, labels, radiusNeighbor, pfile] for s, pfile in zip(inputSets, pfiles)]
 		rval = pool.map_async(radiusKNN, cmds)
 		pool.close()
 		pool.join()
 		
-		
+		"""
 		results = []
 		for inputSet in inputSets:
 			results.append(radiusKNN([inputSet, labels, radiusNeighbor]))
-		"""
 		
 		# step-by-step radiusKNN
 		cmds = [[s, labels, radiusNeighbor, pfile] for s, pfile in zip(inputSets, pfiles)]
@@ -1170,6 +1169,7 @@ class ContigSpace(nx.Graph):
 		for i, cmd in enumerate(cmds):
 			print 'radiusKNN #'+str(i+1)
 			radiusKNN(cmd)
+		"""
 		
 		# interpret the results
 		for pfile in pfiles:
