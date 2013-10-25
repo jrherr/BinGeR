ebf1ddfadeb49da21b77aa657ff07ff276766ca3
chengwei luo <luo.chengwei@icloud.com>
2013-10-25 15:32:04 -0400

    fix typo

--- src/BinGeR.py
+++ src/BinGeR.py
@@ -481,7 +481,7 @@ def main(argv = sys.argv[1:]):
 	utilities.outputBins(projInfo, options)
 	
 	# get all the reads for the bins
-	utilities.extractReadsForBins(ProjInfo, options)
+	utilities.extractReadsForBins(projInfo, options)
 	
 	total_finish_time = time()
 	sys.stdout.write("BinGeR finished at %s\n"%(ctime()))
